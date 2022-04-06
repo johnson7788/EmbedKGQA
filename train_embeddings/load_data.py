a3e29fcb3a7cf755a24da1aefa7e93d5d2e92b68
johnson <johnson@macbook.com>
2022-04-06 11:10:12 +0800

    2022-04-06_11:10

--- train_embeddings/load_data.py
+++ train_embeddings/load_data.py
@@ -15,14 +15,26 @@ class Data:
                 if i not in self.train_relations]
 
     def load_data(self, data_dir, data_type="train", reverse=False):
+        """
+        加载数据
+        :param data_dir:'../data/MetaQA/'
+        :type data_dir:
+        :param data_type:
+        :type data_type:
+        :param reverse:  True
+        :type reverse:bool
+        :return:
+        :rtype:
+        """
         file_name = "%s%s.txt" % (data_dir, data_type)
         print(f"开始读取数据集文件: {file_name}")
         assert os.path.exists(file_name), f"数据文件{file_name}不存在"
         with open(file_name, "r") as f:
-            data = f.read().strip().split("\n")
-            data = [i.split('\t') for i in data]
-            if reverse:
-                data += [[i[2], i[1]+"_reverse", i[0]] for i in data]
+            data = f.read().strip().split("\n")  # 所有数据, 数据条数是133582条
+            data = [i.split('\t') for i in data]  # list格式，其中一条数据格式 ['The Prowler', 'starred_actors', 'Evelyn Keyes']
+            if reverse:  # True
+                reverse_data = [[i[2], i[1]+"_reverse", i[0]] for i in data] # 其中一条数据是： ['Evelyn Keyes', 'starred_actors_reverse', 'The Prowler']
+                data += reverse_data  # 扩充数据，数据的格式是, data数据条数变成 133582* 2 = 267164条
         return data
 
     def get_relations(self, data):
