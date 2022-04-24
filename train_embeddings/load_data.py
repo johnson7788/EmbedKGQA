26c173183526f36c8dfc20aa3aef401601e4682c
johnson <johnson@macbook.com>
2022-04-24 09:16:07 +0800

    2022-04-24_09:16

--- train_embeddings/load_data.py
+++ train_embeddings/load_data.py
@@ -1,46 +1,36 @@
 import json
-import os
+
 
 class Data:
+
     def __init__(self, data_dir="data/FB15k-237/", reverse=False):
+        #load_data，对三元组数据进行处理，头实体和尾实体位置调换，三元组个数加倍
         self.train_data = self.load_data(data_dir, "train", reverse=reverse)
         self.valid_data = self.load_data(data_dir, "valid", reverse=reverse)
         self.test_data = self.load_data(data_dir, "test", reverse=reverse)
+        # 将训练数据，开发数据，测试数据放在一起
         self.data = self.train_data + self.valid_data + self.test_data
+        # 将所有的实体放在一起
         self.entities = self.get_entities(self.data)
-        self.train_relations = self.get_relations(self.train_data)
+        # 收集三个训练集的中关系
+        self.train_relations = self.get_relations(self.train_data)#
         self.valid_relations = self.get_relations(self.valid_data)
         self.test_relations = self.get_relations(self.test_data)
-        self.relations = self.train_relations + [i for i in self.valid_relations \
-                if i not in self.train_relations] + [i for i in self.test_relations \
-                if i not in self.train_relations]
+        # 将所有的关系放在一起
+        self.relations = self.train_relations + [i for i in self.valid_relations if i not in self.train_relations] + [i for i in self.test_relations if i not in self.train_relations]
 
-    def load_data(self, data_dir, data_type="train", reverse=False, isjson=False):
-        """
-        加载数据
-        :param data_dir:'../data/MetaQA/'
-        :type data_dir:
-        :param data_type:
-        :type data_type:
-        :param reverse:  True
-        :type reverse:bool
-        :return:
-        :rtype:
-        """
-        if isjson:
-            file_name = "%s%s.json" % (data_dir, data_type)
-            with open(file_name, "r") as f:
-                data = json.load(f)
-        else:
-            file_name = "%s%s.txt" % (data_dir, data_type)
-            print(f"开始读取数据集文件: {file_name}")
-            assert os.path.exists(file_name), f"数据文件{file_name}不存在"
-            with open(file_name, "r") as f:
-                data = f.read().strip().split("\n")  # 所有数据, 数据条数是133582条
-                data = [i.split('\t') for i in data]  # list格式，其中一条数据格式 ['The Prowler', 'starred_actors', 'Evelyn Keyes']
-                if reverse:  # True
-                    reverse_data = [[i[2], i[1]+"_reverse", i[0]] for i in data] # 其中一条数据是： ['Evelyn Keyes', 'starred_actors_reverse', 'The Prowler']
-                    data += reverse_data  # 扩充数据，数据的格式是, data数据条数变成 133582* 2 = 267164条
+    def load_data(self, data_dir, data_type="train", reverse=False,isjson=True):
+        '''
+        加载三元组数据
+        '''
+        file_name = "%s%s.json" % (data_dir, data_type)
+        with open(file_name,"r") as f:
+            data = json.load(f)
+        if reverse:  # reverse 为true
+            # 头实体和尾实体调换位置,并加入到原data数据里，data数量 133582*2 = 267164个，格式不变
+            data += [[i[2], i[1] + "_reverse", i[0]] for i in data]
+        # strip 去掉多余\n和空格
+        data = [[x.strip() for x in i] for i in data]
         return data
 
     def get_relations(self, data):
--- train_embeddings/main.py
+++ train_embeddings/main.py
@@ -9,8 +9,10 @@ import argparse
 from tqdm import tqdm
 import os
 
+
 class Experiment:
-    def __init__(self, learning_rate=0.0005, ent_vec_dim=200, rel_vec_dim=200, 
+
+    def __init__(self, learning_rate=0.0005, ent_vec_dim=200, rel_vec_dim=200,
                  num_iterations=500, batch_size=128, decay_rate=0., cuda=False, 
                  input_dropout=0.3, hidden_dropout1=0.4, hidden_dropout2=0.5,
                  label_smoothing=0., outfile='tucker.model', valid_steps=1, loss_type='BCE', do_batch_norm=1,
@@ -29,7 +31,7 @@ class Experiment:
         self.model = model
         self.l3_reg = l3_reg
         self.loss_type = loss_type
-        self.load_from = load_from  # 加载模型
+        self.load_from = load_from
         if do_batch_norm == 1:
             do_batch_norm = True
         else:
@@ -217,22 +219,32 @@ class Experiment:
         :rtype:
         """
         torch.set_num_threads(2)
+        # 记录最佳验证
         best_valid = [0, 0, 0, 0, 0]
+        # 记录最佳测试
         best_test = [0, 0, 0, 0, 0]
-        self.entity_idxs = {d.entities[i]:i for i in range(len(d.entities))}   #实体到id的映射
-        self.relation_idxs = {d.relations[i]:i for i in range(len(d.relations))}  #关系到id的映射
+        # 获取实体的下标43234个 188万个实体
+        self.entity_idxs = {d.entities[i]:i for i in range(len(d.entities))}
+        # 获取关系的下标18个  1000多个关系
+        self.relation_idxs = {d.relations[i]:i for i in range(len(d.relations))}
+        # 打开实体字典，'../data/fbwq_full/entities.dict' 写入实体值和id
         f = open('../data/' + self.dataset +'/entities.dict', 'w')
         for key, value in self.entity_idxs.items():
             f.write(key + '\t' + str(value) +'\n')
         f.close()
+        #打开关系字典，../data/fbwq_full/relations.dict'写入关系和id
         f = open('../data/' + self.dataset + '/relations.dict', 'w')
         for key, value in self.relation_idxs.items():
             f.write(key + '\t' + str(value) +'\n')
         f.close()
+        #train_data 训练实体三元组数据转成id向量表示
         train_data_idxs = self.get_data_idxs(d.train_data)
-        print("训练的样本个数: %d" % len(train_data_idxs))
+        # 多少个训练数据267164
+        print("训练的三元组数据个数是: %d" % len(train_data_idxs))
+        # 多少个实体和关系
         print('实体个数: %d' % len(self.entity_idxs))
         print('关系个数: %d' % len(self.relation_idxs))
+        # 选择模型 实体E：Embedding(1886683, 400, padding_idx=0)，关系R：Embedding(1144, 400, padding_idx=0)
         model = TuckER(d, self.ent_vec_dim, self.rel_vec_dim, **self.kwargs)
         model.init()
         if self.load_from != '':
@@ -244,8 +256,9 @@ class Experiment:
         opt = torch.optim.Adam(model.parameters(), lr=self.learning_rate)
         if self.decay_rate:
             scheduler = ExponentialLR(opt, self.decay_rate)
-
+        # 将训练的三元组格式， (214577, 387, 1886604) 转成 （6357，440）：[244510]
         er_vocab = self.get_er_vocab(train_data_idxs)
+        # er_vocab_pairs：[（6357，440）,.....] 由头实体和关系的编码id组成
         er_vocab_pairs = list(er_vocab.keys())
 
         print("开始训练...")
@@ -288,41 +301,43 @@ class Experiment:
                     if valid_mrr >= best_valid[0]:
                         best_valid = valid
                         best_test = test
-                        print('Validation MRR increased.')
-                        print('保存模型中...')
+                        # print('Validation MRR increased.')
+                        print('验证的MRR增加.')
+                        print('保存模型...')
                         self.write_embedding_files(model)
                         print('模型保存完成!')
                     
-                    print('Best valid:', best_valid)
-                    print('Best Test:', best_test)
-                    print('Dataset:', self.dataset)
-                    print('Model:', self.model)
+                    print('最好评估结果:', best_valid)
+                    print('最好测试结果:', best_test)
+                    print('数据集:', self.dataset)
+                    print('模型:', self.model)
 
                     print(time.time()-start_test)
                     print('Learning rate %f | Decay %f | Dim %d | Input drop %f | Hidden drop 2 %f | LS %f | Batch size %d | Loss type %s | L3 reg %f' % 
                         (self.learning_rate, self.decay_rate, self.ent_vec_dim, self.kwargs["input_dropout"], 
                          self.kwargs["hidden_dropout2"], self.label_smoothing, self.batch_size,
-                         self.loss_type, self.l3_reg))        
-           
+                         self.loss_type, self.l3_reg))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset", type=str, default="MetaQA", nargs="?",
-                    help="选择使用哪种数据集 FB15k, FB15k-237, WN18 or WN18RR.")
+    parser.add_argument("--dataset", type=str, default="waishen", nargs="?",
+                    help="使用哪个数据集: waishen,fbwq_full, FB15k-237, MetaQA or WN18RR.")
     parser.add_argument("--num_iterations", type=int, default=500, nargs="?",
-                    help="迭代次数")
+                    help="迭代的次数.")
     parser.add_argument("--batch_size", type=int, default=128, nargs="?",
-                    help="Batch size.")
+                    help="批次大小.")
     parser.add_argument("--lr", type=float, default=0.0005, nargs="?",
-                    help="Learning rate.")
+                    help="学习率")#
     parser.add_argument("--model", type=str, default='ComplEx', nargs="?",
-                    help="支持， DistMult，SimplE，ComplEx，RESCAL，TuckER")
+                    help="使用训练的模型:Model.ComplEx，Rotat3，SimplE，DistMult，RESCAL，TuckER")
     parser.add_argument("--dr", type=float, default=1.0, nargs="?",
-                    help="Decay rate.")
-    parser.add_argument("--edim", type=int, default=200, nargs="?", help="实体嵌入维度")
-    parser.add_argument("--rdim", type=int, default=200, nargs="?", help="关系嵌入维度")
+                    help="衰减率.")
+    parser.add_argument("--edim", type=int, default=200, nargs="?",
+                    help="实体嵌入的维度.")
+    parser.add_argument("--rdim", type=int, default=200, nargs="?",
+                    help="关系嵌入的维度.")
     parser.add_argument("--cuda", type=bool, default=True, nargs="?",
-                    help="是否使用GPU")
+                    help="是否使用显卡.True,False ")
     parser.add_argument("--input_dropout", type=float, default=0.3, nargs="?",
                     help="输入层的dropout")
     parser.add_argument("--hidden_dropout1", type=float, default=0.4, nargs="?",
@@ -336,11 +351,11 @@ if __name__ == '__main__':
     parser.add_argument("--valid_steps", type=int, default=1, nargs="?",
                     help="Epochs before u validate")
     parser.add_argument("--loss_type", type=str, default='BCE', nargs="?",
-                    help="损失类型")
+                    help="损失函数")
     parser.add_argument("--do_batch_norm", type=int, default=1, nargs="?",
-                    help="是否做批归一化 or not (0, 1)")
+                    help="是否使用批次归一化 (0, 1)")
     parser.add_argument("--l3_reg", type=float, default=0.0, nargs="?",
-                    help="l3 reg hyperparameter")
+                    help="l3 reg超参数")
     parser.add_argument("--load_from", type=str, default='', nargs="?",
                     help="是否从state dict加载模型")
 
@@ -352,15 +367,17 @@ if __name__ == '__main__':
     np.random.seed(seed)
     torch.manual_seed(seed)
     if torch.cuda.is_available:
-        torch.cuda.manual_seed_all(seed)
-    # 加载数据
+        torch.cuda.manual_seed_all(seed) ##
+    # data类，处理实体和关系的表示形式
     d = Data(data_dir=data_dir, reverse=True)
-    experiment = Experiment(num_iterations=args.num_iterations, batch_size=args.batch_size, learning_rate=args.lr, 
+    #初始化所有的参数
+    experiment = Experiment(num_iterations=args.num_iterations, batch_size=args.batch_size, learning_rate=args.lr,
                             decay_rate=args.dr, ent_vec_dim=args.edim, rel_vec_dim=args.rdim, cuda=args.cuda,
                             input_dropout=args.input_dropout, hidden_dropout1=args.hidden_dropout1, 
                             hidden_dropout2=args.hidden_dropout2, label_smoothing=args.label_smoothing, outfile=args.outfile,
                             valid_steps=args.valid_steps, loss_type=args.loss_type, do_batch_norm=args.do_batch_norm,
                             dataset=args.dataset, model=args.model, l3_reg=args.l3_reg, load_from=args.load_from)
+
     experiment.train_and_eval()
                 
 
